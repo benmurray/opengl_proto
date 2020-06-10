d4480759ecdcf94dc14f55be4044d37e0023ad41
Ben Murray <ben.h.murray@gmail.com>
2020-06-10 16:58:43 -0500

    Add Texture

--- src/Application.cpp
+++ src/Application.cpp
@@ -12,6 +12,7 @@
 #include "IndexBuffer.h"
 #include "VertextArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 
 int main(void)
@@ -46,10 +47,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     float positions[] = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.5f,  0.5f,
-        -0.5f,  0.5f,
+        -0.5f, -0.5f, 0.0f, 0.0f,
+         0.5f, -0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f, 1.0f
     };
 
     unsigned int indices[] = {
@@ -57,10 +58,14 @@ int main(void)
         2, 3, 0
     };
 
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
     VertexArray* va = new VertexArray();
-    VertexBuffer* vb = new VertexBuffer(positions, 2 * 4 * sizeof(float));
+    VertexBuffer* vb = new VertexBuffer(positions, 4 * 4 * sizeof(float));
 
     VertexBufferLayout layout;
+    layout.Push<float>(2);
     layout.Push<float>(2);
     va->AddBuffer(*vb, layout);
 
@@ -72,6 +77,10 @@ int main(void)
     Shader* shader = new Shader("res/shaders/Basic.shader");
     shader->Bind();
     shader->setUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+	Texture texture("res/textures/fine.jpg");
+	texture.Bind();
+	shader->setUniform1i("u_Texture", 0);
 
     va->Unbind();
     vb->Unbind();
@@ -95,7 +104,7 @@ int main(void)
 
         renderer.Draw(*va, *ib, *shader);
 
-        GLCall(glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, nullptr));
+        //GLCall(glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, nullptr));
 
 
         if (r > 1.0f)
